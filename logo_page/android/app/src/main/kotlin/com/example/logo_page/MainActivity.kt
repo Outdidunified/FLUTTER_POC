3b01ca4af45e6f67e0812f8a14576d23d6bdb5f0
ShreegouriK <shreegourikulkarni24@gmail.com>
2024-11-21 10:37:51 +0530

    updated

--- logo_page/android/app/src/main/kotlin/com/example/logo_page/MainActivity.kt
+++ logo_page/android/app/src/main/kotlin/com/example/logo_page/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package com.example.logo_page
 
 import io.flutter.embedding.android.FlutterActivity
 
--- network_api/android/app/src/main/kotlin/com/example/network_api/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.network_api
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
